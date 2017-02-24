a362405ebacb522fe19721f5753af47ab9932266
Unknown <alexu740@gmail.com>
2017-02-24 10:37:50 +0100

    frontend - album access routes

--- public/content/albumView/albumView.routes.js
+++ public/content/albumView/albumView.routes.js
@@ -3,7 +3,7 @@ angular.module('myApp')
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/')
     $stateProvider.state('album', {
-        url: '/album',
+        url: '/album/:id',
         templateUrl: './content/albumView/albumView.content.html',
         controller: 'albumViewController'
     })
